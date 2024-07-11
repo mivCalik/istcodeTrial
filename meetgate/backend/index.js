ab284943a2ad892e0cd9be9e6334f29b90340a8d
Merve Çalık <57195581+mivCalik@users.noreply.github.com>
2024-07-11 21:34:57 +0300

    Update index.js

--- meetgate/backend/index.js
+++ meetgate/backend/index.js
@@ -63,4 +63,38 @@ app.post('/users', upload.single('profilePicture'), (req, res) => {
   res.redirect('/users');
 });
 
+app.patch('/user/:idnum', (req, res)=>{
+  let existing = usersData.find((obj) => obj.id == req.params["idnum"]);
+  console.log(existing);
+  const replacement = {
+    id:parseInt(req.params["idnum"]),
+    firstName: req.body.firstName || existing.firstName,
+    lastName: req.body.lastName || existing.lastName,
+    email: req.body.email || existing.email,
+    birthDate: req.body.birthDate || existing.birthDate,
+    linkedin: req.body.linkedin ||existing.linkedin,
+    github: req.body.github ||existing.github,
+    twitter: req.body.twitter || existing.twitter,
+  }
+
+  const index = usersData.findIndex((user) => user.id == req.params["idnum"]);
+  usersData[index] = replacement;
+  console.log(replacement);
+  res.redirect('/users');
+})
+
+app.delete('/user/:idnum', (req, res)=>{
+  let existing = usersData.find((obj) => obj.id == req.params.idnum); 
+
+  const index = usersData.findIndex((user) => user.id == req.params.idnum);
+  if(index > -1){
+    usersData.splice(index,1);
+    res.redirect('/users');
+  }else{
+    res.status(404)
+      .json({error:`User with id: ${req.params["idnum"]} not found.`});
+    }
+  }
+  );
+
 app.listen(port, () => console.log(`Server running on port ${port}!`));
